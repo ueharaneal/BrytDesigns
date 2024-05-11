e280f4b4eb0eeee1fb83f48c83b05973d0c4e835
Neal Uehara <79432460+ueharaneal@users.noreply.github.com>
2024-05-10 19:23:03 -0700

    need to fix formatter

--- src/app/multiStepForm/MultiStepForm.tsx
+++ src/app/multiStepForm/MultiStepForm.tsx
@@ -1,6 +1,6 @@
 
 import {useState } from "react";
-
+import { z} from "zod";
 import Step1 from "./Step1" 
 import Step2 from "./Step2"
 import Step3 from "./Step3"
@@ -13,13 +13,86 @@ import Step3 from "./Step3"
     const buttonClass = "bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded";
     const mutedClass = "bg-border text-white font-bold py-2 px-4 rounded";
 
+     type formSchema = { 
+        account: {
+            firstName: string,
+            lastName: string,
+            userName: string,
+            email: string,
+            password: string,
+            confirmPassword: string
+        },
+        address: {
+            street: string,
+            apartment: string,
+            country: string,
+            city: string,
+            state: string,
+            zip: string
+            company: string
+            phone: string  
+        },
+        preferences: {
+            theme: string,
+            language: string
+        }
+      }    
+      
+      //creating the initial state of the form
+        const initialFormState: formSchema = {
+            account: {
+                firstName: "",
+                lastName: "",
+                userName: "",
+                email: "",
+                password: "",
+                confirmPassword: ""
+            },
+            address: {
+                street: "",
+                apartment: "",
+                country: "",
+                city: "",
+                state: "",
+                zip: "",
+                company: "",
+                phone: ""
+            },
+            preferences: {
+                theme: "",
+                language: ""
+            }
+        }
+
     const [formStep, setFormStep] = useState(0)
     const [formTitle, setFormTitle] = useState("Account")
+
+    //creating the the object for the titles
+    //honestly i should have put this with the step components 
+    const formTitles : Record<number, string> = {
+        0: "Account",
+        1: "Address",
+        2: "Preferences"
+    }
+    //creating handle prev and next Step handler
+    const handleNextStep = () => {
+        setFormStep(prevStep => prevStep + 1)
+        setFormTitle(formTitles[formStep])
+    }
+    const handlePrevStep = () => {
+        setFormStep(prevStep => prevStep - 1)
+        setFormTitle(formTitles[formStep])
+    }
+
+    const handleFormSubmit = () => {
+        console.log("Form submitted")
+    }
     return (
         <>
-            <div className="w-[550px] h-[613px] bg-card shadow-lg rounded-lg flex flex-col justify-center items-center my-4">
+            <div className="w-[650px] h-[713px] bg-card shadow-lg rounded-lg flex flex-col justify-center items-center my-4">
                 <div className="w-full text-center font-bold text-3xl rounded-lg py-4">{formTitle}</div>
-            <div className="w-[530px] h-[613px] bg-white flex flex-col justify-between">
+            <div className="w-[630px] h-[713px] bg-white flex flex-col items-center justify-between">
+                <div className="text-center">This is the progress bar </div>
                 {
                     formStep === 0 && <Step1 />
                 }
@@ -30,9 +103,9 @@ import Step3 from "./Step3"
                     formStep === 2 && <Step3 />
                 }
 
-                <div className="flex flex-row justify-around my-7">
-                {formStep !== 0 ? <button  className= {`${buttonClass} `}  onClick={() => setFormStep(formStep - 1)}>Back</button> : <button className= {`${mutedClass}`} disabled onClick={() => setFormStep(formStep - 1)}>Back</button>}
-                {formStep !== 2 ? <button className= {`${buttonClass} `} onClick={() => setFormStep(formStep + 1)}>Next</button> : <button className= {`${mutedClass} `} disabled onClick={() => setFormStep(formStep + 1)}>Next</button>}
+                <div className="flex flex-row items-center  w-full justify-around my-7">
+                {formStep !== 0 ? <button  className= {`${buttonClass} `}  onClick={handlePrevStep}>Back</button> : <button className= {`${mutedClass}`} disabled onClick={() => setFormStep(formStep - 1)}>Back</button>}
+                {formStep !== 2 ? <button className= {`${buttonClass} `} onClick={handleNextStep}>Next</button> : <button className= {`${buttonClass} `}  onClick={handleFormSubmit}>Register</button>}
                 </div>
             </div> 
 
--- src/app/multiStepForm/Step1.tsx
+++ src/app/multiStepForm/Step1.tsx
@@ -4,8 +4,37 @@ import { useState } from 'react';
 
 export default function Step1() {
     return (
-        <div>   
-            Step 1 form 
-        </div>
+        <form className='flex flex-col gap-y-3 w-5/6'>   
+            <div className='flex flex-row justify-between'>
+                <div className='flex flex-col my-1'>
+                <label htmlFor="firstName">First Name</label>
+                <input type="text" id='firstName' name='firstName' className='border rounded-lg px-3 py-2 focus:outline-none focus:ring-black focus:border-black'/>
+                </div>
+                <div className='flex flex-col my-1'>
+                <label htmlFor="lastName">Last Name</label>
+                <input type="text" id='lastName' name='lastName' />
+                </div>
+            </div>
+            <div className='flex flex-col my-1'>
+                <label htmlFor="userName">User Name</label>
+                <input type="text" id='userName' name='userName' />
+            </div>
+            <div className='flex flex-col my-1'>
+                <label htmlFor="email">Email</label>
+                <input type="email" id='email' name='email' />
+            </div>
+            <div>
+                <div className='flex flex-col my-1'>
+                    <label htmlFor="password">Password</label>
+                    <input type="password" id='password' name='password' />
+                </div>
+                <div className='flex flex-col my-1'>
+
+                <label htmlFor="confirmPassword">Confirm Password</label>
+                <input type="password" id='confirmPassword' name='confirmPassword' />  
+
+                </div>
+            </div>
+        </form>
     )
 }
